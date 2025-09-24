bc6a2b3c72203de402dc2a83b8a9d206ad15e99a
brian d foy <briandfoy@pobox.com>
2025-09-24 02:37:22 -0400

    initial creation

---
+++ lib/USPS/API.pm
@@ -0,0 +1,65 @@
+package USPS::API;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+USPS::API - The United States Postal Service APIs
+
+=head1 SYNOPSIS
+
+	use USPS::API;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/
+
+=head1 AUTHOR
+
+brian d foy, C<< <briandfoy@pobox.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2025, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
